78058b93a220970b29550284a92ea5dcbb5f5e21
cchiu28 <scorpiusdx@gmail.com>
2019-04-01 11:48:45 -0400

    Push 2nd API that geocodes string input to coordinates

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
 	public static void main(String[] args) {
-		gui.execute(args);
+//		gui.execute(args);
+		String url = "brooklyn college";
+		getData data = new getData();
+		try {
+			data.geoCode(url);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
