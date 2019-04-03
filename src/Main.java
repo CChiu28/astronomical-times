561f06b5d173fa62879dbbda673510e232f49549
Pentium4 <jiaannwu@gmail.com>
2019-04-03 17:43:42 -0400

    changed main to use both functions

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,11 @@ public class Main {
 		String url = "brooklyn college";
 		getData data = new getData();
 		try {
-			data.geoCode(url);
+			Map<String,String> result = new HashMap<String,String>();
+			// Using geoCode to generate a map that will be used in sendGet to retrieve the proper formatted information
+			result = data.geoCode(url);
+			String formatted_info = data.sendGET(result.get("lon"), result.get("lat"), /*date to be added*/);
+			System.out.println(formatted_info);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
