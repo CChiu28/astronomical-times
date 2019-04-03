8a6ee783743cf1b77c4f53c51855f0ecf0347558
Dominik_Kopec <megakopec@gmail.com>
2019-04-03 10:04:28 -0400

    Updated GUI

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-//		gui.execute(args);
+		gui.execute(args);
 		String url = "brooklyn college";
 		getData data = new getData();
 		try {
